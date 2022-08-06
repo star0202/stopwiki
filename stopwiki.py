b1e884eafb5ecf92c7813b13eb74405aa6ec905e
Whitetiger <teowhite0423@gmail.com>
2022-08-06 18:26:38 +0900

    Create stopwiki.py

---
+++ stopwiki.py
@@ -0,0 +1,35 @@
+import discord
+import re
+import time
+
+client = discord.Client(intents=discord.Intents.all())
+
+
+@client.event
+async def on_ready():
+    print("Logged in as")
+    print(client.user.name)
+    print(client.user.id)
+    print("------")
+    await client.change_presence(
+        status=discord.Status.online,
+        activity=discord.Game("검열"),
+    )
+
+
+p = re.compile(
+    "(https?:\/\/(ko[.])?wikipedia[.]org\/wiki(\/[^\s]+)?)|(https?:\/\/w[.]wiki(\/[^\s]+)?)"
+)
+
+
+@client.event
+async def on_message(msg):
+    if p.search(msg.content):
+        a = await msg.channel.send("위키백과 관련 대화는 제한됩니다.")
+        await msg.delete()
+        time.sleep(5)
+        await a.delete()
+        return
+
+
+client.run(TOKEN)
